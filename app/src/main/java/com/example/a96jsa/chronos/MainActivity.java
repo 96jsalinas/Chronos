56fa741efbb0abfacbec8289462813fb0895a7bd
Josu <96jsalinas@gmail.com>
2018-03-25 14:10:48 +0300

    Added button click listener to create toast indicating written category

--- app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
+++ app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.a96jsa.chronos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText category = findViewById(R.id.category);
+        final Button button = findViewById(R.id.record);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Category: "+category.getText().toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
+
+
+
 }
