7df6e761334934ded75ec4fcaac3ab965adfcf33
Josu <96jsalinas@gmail.com>
2018-03-27 17:24:52 +0300

    Created new activity for main screen, added side menu to main activity

--- app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
+++ app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.a96jsa.chronos;
 
+import android.content.Intent;
 import android.os.SystemClock;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -12,11 +19,38 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private boolean recording = false;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_apps_black_24dp);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        Intent homeIntent = new Intent(getBaseContext(),MainScreen.class);
+                        startActivity(homeIntent);
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+
         final EditText category = findViewById(R.id.category);
         final Button button = findViewById(R.id.record);
         final Chronometer simpleChronometer = findViewById(R.id.simpleChronometer);
@@ -40,6 +74,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
