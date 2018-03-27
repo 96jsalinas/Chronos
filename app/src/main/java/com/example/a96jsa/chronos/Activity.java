236fe569480bfb5deb9cee1a44d499951d813d73
Kevin Mendez <kevinrmendez@gmail.com>
2018-03-28 00:59:11 +0300

    CRUD implemented, main activity layout changed for testing

--- app/src/main/java/com/example/a96jsa/chronos/Activity.java
+++ app/src/main/java/com/example/a96jsa/chronos/Activity.java
@@ -9,6 +9,47 @@ import java.util.Date;
 
 public class Activity {
     private long _id;
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public Date getRecordingDate() {
+        return recordingDate;
+    }
+
+    public void setRecordingDate(Date recordingDate) {
+        this.recordingDate = recordingDate;
+    }
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public static int getDefaultTime() {
+        return DEFAULT_TIME;
+    }
+
+    public static void setDefaultTime(int defaultTime) {
+        DEFAULT_TIME = defaultTime;
+    }
+
     private String activityName;
     private Date recordingDate;
     private int totalTime;
--- app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
+++ app/src/main/java/com/example/a96jsa/chronos/MainActivity.java
@@ -17,10 +17,23 @@ import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean recording = false;
     private DrawerLayout mDrawerLayout;
+     private SQLiteDatabase db;
+
+    @BindView(R.id.add_category) Button add_category_button;
+    @BindView(R.id.category) EditText category;
+    @BindView(R.id.remove_category) EditText remove_category_et;
+   // final EditText category = findViewById(R.id.category);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +46,11 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_apps_black_24dp);
 
+        ButterKnife.bind(this);
+
+
         Dbhelper dbhelper = new Dbhelper(getApplicationContext());
-        SQLiteDatabase db = dbhelper.getWritableDatabase();
+         db = dbhelper.getWritableDatabase();
 
         if(db != null){
             Toast.makeText(this, "database created", Toast.LENGTH_LONG).show();
@@ -58,10 +74,8 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
-
-
-        final EditText category = findViewById(R.id.category);
         final Button button = findViewById(R.id.record);
+
         final Chronometer simpleChronometer = findViewById(R.id.simpleChronometer);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -76,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     simpleChronometer.stop();
                     long elapsedMillis = SystemClock.elapsedRealtime() - simpleChronometer.getBase();
+                    int seconds = (int)elapsedMillis/1000;
                     Toast toast = Toast.makeText(getApplicationContext(), "Elapsed time (ms): " + elapsedMillis, Toast.LENGTH_SHORT);
                     toast.show();
                     recording = false;
@@ -83,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
 
 
     }
@@ -95,6 +112,41 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.add_category)
+    public void addCategory(){
+        String categoryName = category.getText().toString();
+        Toast.makeText(getApplicationContext(),categoryName,Toast.LENGTH_LONG).show();
+        Activity activity = new Activity(categoryName);
+        cupboard().withDatabase(db).put(activity);
+        Toast.makeText(getApplicationContext(),"data written in database",Toast.LENGTH_LONG).show();
+
+    }
+    @OnClick(R.id.get_category)
+    public void getCategory(){
+        try{
+            Activity result = cupboard().withDatabase(db).query(Activity.class).get();
+            Toast.makeText(getApplicationContext(),result.getActivityName(),Toast.LENGTH_LONG).show();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"no cateogry added",Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+    }
+    @OnClick(R.id.remove_category_button)
+    public void removeCategory(){
+        String query = remove_category_et.getText().toString();
+        if(query.isEmpty()){
+            Toast.makeText(getApplicationContext(),query + " specify which category you want to remove",Toast.LENGTH_LONG).show();
+        }else {
+            cupboard().withDatabase(db).delete(Activity.class,"activityName = ?", query);
+            Toast.makeText(getApplicationContext(),query + " removed from database",Toast.LENGTH_LONG).show();
+        }
+
     }
 
 
